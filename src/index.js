896e1a6a2d9acf23818de9bf987ff441c90673a7
Andrey <aimdragman2@yandex.ru>
2021-02-28 05:36:41 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+	if (array === undefined || array.length === 0) {
+		return 0;
+	}
+	return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+		return 0;
+	}
+	return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array.length === 0) {
+		return 0;
+	}
+	const result = array.reduce((sum, current) => {
+		return sum + current;
+	});
+	return result / array.length;
 }
